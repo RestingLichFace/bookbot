79f0b01d9592d4d7564525a53de183de035761f6
RestingLichFace <eternalresolution@gmail.com>
2024-05-22 19:09:33 -0400

    overhauled main function to reflect changes made to sorting and counting functions

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    word_count = count_words(text)
-    print(f"{word_count} words found in the document.")
+    num_words = count_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"---Begin report of {book_path}---")
+    print(f"{num_words} words found in the document.")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("---End report---")
 
 
 def get_book_text(path):
